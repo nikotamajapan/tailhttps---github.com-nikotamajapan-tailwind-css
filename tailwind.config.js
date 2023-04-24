8ed483a0f543c46128d63a5defea32786f33754a
nikomb3 <nikotamajapan@gmail.com>
2023-04-25 03:30:57 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    backgroundImage:{
+      "tutorial-bg": "url('../src/img/main-bg.jpg')",
+    },
+  },
+  plugins: [],
+}
